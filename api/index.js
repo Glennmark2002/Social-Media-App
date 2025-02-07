6e4511fb62a55a0f0ff9e9789bbb1030e5b5415b
Glennmark2002 <glennmark.culibra@gmail.com>
2025-02-07 10:38:35 +0800

    test api

--- api/index.js
+++ api/index.js
@@ -1,9 +1,30 @@
 import express from 'express'; 
+import User from './models/user.model.js';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
 
 const app = express(); 
+app.use(cors());
+app.use(express.json())
+
+mongoose.connect('mongodb+srv://Glennmark:Glennmark09@socialmediaapp.su1ai.mongodb.net/User?retryWrites=true&w=majority&appName=SocialMediaApp')
+.then(() => console.log('Connected'))
+.catch((err) => console.log(err))
+
 
 app.get("/", (req, res) => {
     res.json("Hello");
+}); 
+
+app.post('/api', async (req, res) => {
+
+    const { username, email } = req.body;
+    const newUser = new User({username, email});
+
+    await newUser.save(); 
+    res.status(201).json({ message: 'User created successfully' });
+
 })
 
 app.listen(3000, () => console.log('Running at port 3000'));
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import axios from 'axios';
+import { useState } from 'react';
 
 function App() {
+
+  const [formData, setFormData] = useState({});
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    axios.post('https://social-media-app-gje5.vercel.app/api', formData)
+    .then((res) => console.log(res))
+    .catch(error => console.log(error))
+
+  }
+
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.id]: e.target.value });
+    console.log(formData);
+  }
+
+
   return (
-    <div>
-      TEST 
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input type="text" id='username' placeholder='Username'        onChange={handleChange}/>
+      <input type="text" id='email' placeholder='Email'       onChange={handleChange}/>
+      <button> Submit </button>
+    </form>
   );
 }
 
