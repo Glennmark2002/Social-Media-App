171a8bf1e6a49dc1a4edece910252c3bf97a7df4
Glennmark2002 <glennmark.culibra@gmail.com>
2025-02-11 12:22:51 +0800

    aaa

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -9,7 +9,7 @@ function Comment({ ref, post, currentUser, fetchComments }) {
   const handleTextInput = async () => {
 
     // axios.post(`http://localhost:3000/api/post/${post._id}`, { text, userId : currentUser._id });
-    axios.post(`https://social-media-app-gje5.vercel.app/api/post/${post._id}`, { text, userId : currentUser._id });
+    await axios.post(`https://social-media-app-gje5.vercel.app/api/post/${post._id}`, { text, userId : currentUser._id });
     setText('');
     let textArea = document.getElementById(post._id);
     textArea.value = '';
